832cf6bbc3f877ebe583cf2f1096a9ea7efa11cd
ydyd0809 <ydyd0809@naver.com>
2025-09-05 23:47:49 +0900

    백준 - 체스칠하기

--- BAEKJOON/INFLEARN/01-05.java
+++ BAEKJOON/INFLEARN/01-05.java
@@ -1,32 +1,15 @@
 package BAEKJOON.INFLEARN;
 import java.util.*;
-
-class Main {
-        public String solution(String str){
-            // 첫번째, 마지막 인덱스에서 비교하여 
-            char[] arr = str.toCharArray();
-            int lt = 0, rt = arr.length -1;
-            while (lt<rt) {
-                if(!Character.isAlphabetic(arr[lt])){
-					lt++;
-                }else if (!Character.isAlphabetic(arr[rt])){
-                     rt--;
-                }else{
-                                    
-                        char tmp = arr[lt];
-                        arr[lt] = arr[rt];
-                        arr[rt] = tmp;
-                        lt++;
-                        rt--;
-                    
-            }}
-            return String.valueOf(arr);
-        }
-        public static void main(String[] args){
-        Main T = new Main();
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(T.solution(str));
-    }
-
+import java.io.*;
+  
+ class Main {
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+    String revStr = new StringBuilder(str).reverse().toString();
+    String answer = "NO";
+    if(str.equalsIgnoreCase(revStr)) answer = "YES";
+    System.out.println(answer);
+    return ;
+  }
 }
